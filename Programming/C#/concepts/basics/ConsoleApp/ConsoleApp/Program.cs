b50ef20fe40b4b4d65c339dd5ca621905f9d2103
cs.mccaleb@gmail.com <cs.mccaleb@gmail.com>
2019-01-11 21:33:51 -0600

    added C# calculator app

--- Programming/C#/concepts/basics/ConsoleApp/ConsoleApp/Program.cs
+++ Programming/C#/concepts/basics/ConsoleApp/ConsoleApp/Program.cs
@@ -6,7 +6,22 @@ namespace ConsoleApp
     {
         static void Main(string[] args) // this is a method called "Main". It is called when the program starts.
         {
-            Console.WriteLine("Hello, this is Uonai.");
+            // Basic console program
+            // Console.WriteLine ("Hello, this is Uonai.");
+            // Console.ReadKey ();
+            // Console.WriteLine ("\nMy favorite color is black");
+            // Console.ReadKey ();
+
+
+            // Calculator program
+            int num01;
+            int num02;
+
+            Console.Write("Type a number to be multiplied: ");
+            num01 = Convert.ToInt32 (Console.ReadLine ());
+            Console.Write("Type another number: ");
+            num02 = Convert.ToInt32(Console.ReadLine ());
+            Console.WriteLine("The result is " + num01 * num02);
             Console.ReadKey();
         }
     }
