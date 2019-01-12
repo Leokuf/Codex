80e0a614ba5a02d02b685d5ccf52e4830840f460
cs.mccaleb@gmail.com <cs.mccaleb@gmail.com>
2019-01-11 21:16:54 -0600

    Adding C# basics.

---
+++ Programming/C#/concepts/basics/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args) // this is a method called "Main". It is called when the program starts.
+        {
+            Console.WriteLine("Hello, this is Uonai.");
+            Console.ReadKey();
+        }
+    }
+}
